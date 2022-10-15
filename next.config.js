c91a8adb387b91d45236b2f90dfa1b39fb3a162d
mediademi <114319898+mediademi@users.noreply.github.com>
2022-10-15 15:40:05 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ module.exports = {
           }
         ],
         permanent: false,
-        destination: 'https://intclassmarketing.com/posts/:path*',
+        destination: 'https://portalimomenti.com/posts/:path*',
       },
     ]
   }
