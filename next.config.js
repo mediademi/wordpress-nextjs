39a5bff43e53524af2310915359808cc85b7499e
mediademi <114319898+mediademi@users.noreply.github.com>
2022-12-11 07:00:51 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,17 +1,30 @@
 module.exports = {
-  async redirects() {
-    return [
-      {
-        source: '/post/:path*',
-        has: [
-          {
-            type: 'query',
-            key: 'fbclid'
-          }
-        ],
-        permanent: false,
-        destination: 'https://portalimomenti.com/posts/:path*',
-      },
-    ]
+  serverRuntimeConfig: {
+    // This will only be available on the server-side
+    baseUrl: 'https://www.telegrafi.com'
+  },
+  publicRuntimeConfig: {
+    // This will be available on both the server-side and the client-side
+    redirectUrl: '/redirect'
+  },
+  webpack: (config) => {
+    // Add a custom `RedirectFromFacebook` middleware to the server bundle
+    config.server.middleware.push('RedirectFromFacebook');
+
+    // Return the modified config object
+    return config;
+  }
+};
+
+// Custom middleware function to redirect the user if they came from facebook.com
+function RedirectFromFacebook(req, res, next) {
+  // Check if the user's referrer is facebook.com
+  if (req.headers.referer && req.headers.referer.startsWith('https://facebook.com')) {
+    // Redirect the user to the specified URL
+    res.writeHead(301, {Location: `${this.publicRuntimeConfig.baseUrl}${this.publicRuntimeConfig.redirectUrl}`});
+    res.end();
+  } else {
+    // Continue to the next middleware function
+    next();
   }
 }
